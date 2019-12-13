a1700a8780f5af91ffaa4d476b165d373dcc9fcf
shashisingh5 <36893987+shashisingh5@users.noreply.github.com>
2019-12-13 18:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,37 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 import plotly.graph_objs as go
+import pandas as pd
+
 
 app = dash.Dash()
+dragonData=pd.read_excel("https://github.com/shashisingh5/dashDummy/blob/master/Dragon_details.xlsx?raw=true");
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
+
+
+    
+print (dragonData['Year']);
+
+trace1 = go.Bar(
+    x=dragonData['Year'],
+    y=dragonData['Won'],
+    name='Goku Won'
+)
+
+trace2 = go.Bar(
+    x=dragonData['Year'],
+    y=dragonData['Loss'],
+    name='Goku Loss'
+)
 
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.8,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
-])
+    dcc.Graph(id='bar_plot',
+              figure=go.Figure(data=[trace1,trace2],
+                               layout=go.Layout(barmode='stack'))
+              )
+    ])
 
-if __name__ == '__main__':
-    app.run_server()
+
+app.run_server()
