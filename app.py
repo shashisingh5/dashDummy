4ebeedb4d749df06d08e1e37ece4a8d2d2362d9e
shashisingh5 <36893987+shashisingh5@users.noreply.github.com>
2019-12-18 12:33:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+from flask import Flask
+import socket
 
+server = Flask(__name__)
 app = dash.Dash()
 dragonData=pd.read_excel("https://github.com/shashisingh5/dashDummy/blob/master/Dragon_details.xlsx?raw=true");
 
@@ -32,5 +36,6 @@ app.layout = html.Div([
               )
     ])
 
+host = socket.gethostbyname(socket.gethostname())
+app.run_server(debug=False, host=host, port = 8080)
 
-app.run_server()
